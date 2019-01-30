e27a01f64bfce2bd98c46254b25c7367f61278f4
Stephen Karpeles <skarpeles@bluechipww.com>
2019-01-29 21:26:10 -0600

    Fonts added

--- wp-content/themes/struere/header.php
+++ wp-content/themes/struere/header.php
@@ -25,34 +25,20 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'struere' ); ?></a>
 
 	<header class="header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$struere_description = get_bloginfo( 'description', 'display' );
-			if ( $struere_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $struere_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+		<div class="header__logo">
+			<a href="">
+				<img src="<?php echo get_stylesheet_directory_uri();?>/img/logo-white.png" alt="Struere">
+			</a>
+		</div>
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'struere' ); ?></button>
+		<nav class="header__nav">			
 			<?php
 			wp_nav_menu( array(
 				'theme_location' => 'menu-1',
 				'menu_id'        => 'primary-menu',
 			) );
 			?>
-		</nav><!-- #site-navigation -->
+		</nav>
 	</header>
 
 	<main>
