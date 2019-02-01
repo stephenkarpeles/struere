7cc4e52519cc370401e7e1a82a5966b139204f12
Stephen Karpeles <skarpeles@bluechipww.com>
2019-02-01 15:24:42 -0600

    About page

--- wp-content/themes/struere/page-about.php
+++ wp-content/themes/struere/page-about.php
@@ -29,6 +29,43 @@ get_header();
 
     </div>
   </section>
+
+  <section class="hraztan">
+    <div class="container">
+      <div class="hraztan__image">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/hraztan.jpg" alt="Hraztan Zeitlian">
+      </div>
+      <div class="hraztan__text">
+        <p><span class="hraztan__name">Hraztan Zeitlian</span> is the Founder and Design Principal of STRUERE Architecture, Los Angeles. He leads design conceptualization and strategy, in addition to overseeing all aspects of the practice. Hraztan brings high-energy design leadership focused on creating Award Winning, sophisticated/innovative design solutions, with a strong collaborative connection to multiple Market Sectors, Disciplines, management goals and technical execution. Prior to STRUERE, Hraztan was Design Leader of DLR Group WWCOT, Director of Design of Leo A Dalys Los Angeles Office and was Director of Design of Genslers Los Angeles offices Entertainment Studio 1. His work experience includes SOM and JohnsonFain.</p>
+        <p>Design is Hraztans passion. His work has garnered many Design Awards, including 3 American Architecture National Design Awards from the Chicago Athenaeum and 2 American Institute of Architects Los Angeles Chapter Awards. In addition, he has received Design Awards from the AIA National Academy of Architecture for Justice, the AIA California Council and multiple AIA Chapters throughout Southern California, as well as DesignAwards from a number of Los Angeles area organizations such as the SCDF. He brings a forward thinking, even experimental approach to project design, he believes in each project there is always an opportunity for innovation. His approach to design is highly-contemporary, inspired by the emerging technologies of communications and media. He thrives on creating truly synergistic, exciting public spaces of interaction.</p>
+        <p>Hraztans work has been published internationally in Domus, LARCA, as well as nationally in Architectural Record, Architect, Form, Contract Design.</p>
+        <p>Hraztan takes leadership in disseminating the great work developed in the office under his design leadership with speaking engagements. In 2011, he was a presenter at the Los Angeles Community College District 9x9x9 Conference. Also in 2011, he gave a Lecture at Woodbury University about the California Architects Practice Act. In 2009, he was a panelist at the Los Angeles AIA Design Build RoundTable. In 2008, he was a Panelist at the AIA Los Angeles / UCLA Event: In the Academic Realm.</p>
+        <p>Hraztan sees the connection to Academia as essential to developing advanced design within the professional world. He has taught design studio at USC and is a frequent critic at Los Angeles Architecture Design Schools such as UCLA, USC, SCIARC, Woodbury University, Otis.</p>
+        <p>He was the Editor of Semiotext(e)/Architecture (1992), a review of experimental architecture and theory. He was member of the LA Architect Magazine Editorial Board (1996-98).</p>
+        <p>His diverse experience includes Commercial, Civic, Educational, Library, Performing Arts, as well as, Housing, Hospitality, Entertainment, Healthcare, Science and Technology, Religion, Masterplan and Urban Design projects.</p>
+        <p>With the hyper-connected world, Hraztan believes that Architecture today is necessarily practiced at a global scale. He has worked on numerous international projects, including in China, Japan and the Middle East.</p>
+        <p>Hraztan Zeitlian is a licensed Architect and a LEED Accredited Professional with an interest in sustainable design and energy efficient building design. Hraztan believes Architects need to exert leadership in expanding public awareness of green building design and help policy makers implement green building policies.</p>
+        <p>In 10/2008 California Governor Arnold Schwarzenegger appointed Hraztan Zeitlian to the California Architects Board, he was reappointed in 12/2010 to a second term. In 07/2009 Los Angeles Mayor Antonio Villaraigosa appointed him to the Mayors Design Advisory Panel.</p>
+        <p>Hraztan was elected in 2007 to the governing board of the American Institute of Architects Los Angeles Chapter. It needs to be noted that the Los Angeles chapter is the second largest chapter of the American Institute of Architects. Currently a member of board of the AIA California Councils Monterey Design Conference, Hraztan was chair of the AIACC Design Awards Committee, and also served as Chair of the AIA LA Design Awards Committee, where he served 2005-7. He is the founder of the Practice Committee and has been active with the AIA LA Political Outreach Committee.</p>
+        <p>Hraztan is past President of the Board of the Southern California Development Forum. He is a past member of the governing Board of the Los Angeles Business Council and is active on its Legislative Affairs Committee. These are two of the premiere networking organizations in Southern California.</p>
+        <p>Hraztan believes in advancing design through research and experimental pursuits.</p>
+        <p>Hraztan has a Masters in Advanced Architectural Design from Columbia University and a Bachelor of Architecture from the Rhode Island School. He has studied Architecture at the American University of Beirut.</p>
+        <p>Foreign Languages include French, Armenian, and familiarity with Arabic.</p>
+        <p><a href="#" target="_blank"><strong>AWARDS PRIOR TO STRUERE &raquo;</strong></a></p>
+      </div>
+    </div>
+  </section>
+
+  <section class="silva">
+    <div class="container">
+      <div class="silva__text">
+        <p><span class="silva__name">Silva Zeitlian</span> is the Founder and Design Principal of STRUERE Architecture, Los Angeles. He leads design conceptualization and strategy, in addition to overseeing all aspects of the practice. Hraztan brings high-energy design leadership focused on creating Award Winning, sophisticated/innovative design solutions, with a strong collaborative connection to multiple Market Sectors, Disciplines, management goals and technical execution. Prior to STRUERE, Hraztan was Design Leader of DLR Group WWCOT, Director of Design of Leo A Dalys Los Angeles Office and was Director of Design of Genslers Los Angeles offices Entertainment Studio 1. His work experience includes SOM and JohnsonFain.</p>
+      </div>
+      <div class="silva__image">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/silva.jpg" alt="Hraztan Zeitlian">
+      </div>
+    </div>
+  </section>
   
 </div>
 
