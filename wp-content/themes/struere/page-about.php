025444209b56bba4efb9832ee5bb2c0e27eee83c
Stephen Karpeles <skarpeles@bluechipww.com>
2019-02-01 14:22:13 -0600

    Add transitions

--- wp-content/themes/struere/page-about.php
+++ wp-content/themes/struere/page-about.php
@@ -8,6 +8,27 @@ get_header();
 ?>
 
 <div class="about">
+  <div class="about__bg"></div>
+
+  <section>
+    <div class="container">      
+      <div class="about__company-info">
+        <h1>Struere Architecture</h1>
+        <p class="intro">We love Architecture, Shared Space and Technology! Our design passion lies where these categories merge. We are drawn to innovative building ideas that solve big problems, ameliorate the human experience and enhance our environment.</p>
+        <h2>Who We Are</h2>
+        <p><strong>Struere is an Architecture and Interiors firm based in West Los Angeles</strong>. Whether to be newly built or preservation, we approach each architectural design project with one purpose – to achieve excellence in design and service.</p> 
+        <p>For us Architecture is a movement for progressive change in the urban environment. Each project is an opportunity for innovation and experimentation.</p>
+        <p>We are well versed in leading edge design trends and forward thinking ideas that encapsulate 21st century places of living, learning, working and playing. Our portfolio demonstrates our experience in sustainability, wellness strategies, media and technology Integration and the creation of vibrant shared spaces of interaction and interactivity.</p> 
+        <h2>Leadership</h2>
+        <p>Struere offers the intimacy of a boutique studio devoted to design that provides personalized service and team continuity, backed by the vast experience the leadership team has acquired while working with the largest firms in Los Angeles. Our leadership consists of Founder Hraztan Zeitlian as the Design Principal for Shell and Core Architecture and Silva Zeitlian as Design principal for Interior Architecture.</p>
+        <p>Struere's leadership history spans three decades working for a wide variety of prestigious clients such as Google, Disney, Sony, LA Rams, Metlife, LA City, LA County, USC, UCLA, LACCD, LAUSD, SMMUSD, ArcLight, AMC, just to name a few.</p>
+        <h2>Work</h2>
+        <p>Our portfolio of successful projects presents excellence in the Commercial, Residential Mixed Use, Civic and Educational Market Sectors. Our Leadership is recognized as innovators in these areas through prestigious Design Awards, including 3 American Architecture National Design Awards from the Chicago Athenaeum and 2 American Institute of Architects Los Angeles Chapter Design Awards,  in addition to Design Awards from the AIA National Academy of Architecture for Justice, the AIA California Council and multiple AIA Chapters throughout Southern California.</p>
+        <p>Our work has been published internationally in Domus, L'ARCA, as well as nationally in Architectural Record, Architect, Form, Contract Design, Architect's Newspaper, etc. </p>
+      </div>
+
+    </div>
+  </section>
   
 </div>
 
---
+++ wp-content/themes/struere/page-homepage.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Template Name: Homepage
+ * @package WordPress
+ */
+
+get_header();
+?>
+
+<p>Content</p>
+
+
+<?php get_footer(); ?>
