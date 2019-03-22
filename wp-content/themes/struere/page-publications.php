2f765ca3c480d2013bbd7d7606985a2b79a2fad2
Stephen Karpeles <skarpeles@bluechipww.com>
2019-03-22 13:03:14 -0500

    Finished design updates

--- wp-content/themes/struere/page-publications.php
+++ wp-content/themes/struere/page-publications.php
@@ -20,12 +20,12 @@ get_header();
     <div class="container">
       <!-- <h2>Online</h2> -->
       <ul>
-        <li><a href="http://www.dezeen.com/2016/10/15/struere-builds-hollywood-home-revive-mid-century-modernism-los-angeles-california/" target="_blank"><span>dezeen.</span> Struere builds Hollywood home to help revive mid-century modernism.</a></li>
-        <li><a href="http://www.archdaily.com/795861/hollywood-hills-residence-struere?utm_medium=email&utm_source=ArchDaily%20List" target="_blank"><span>ArchDaily.</span> Hollywood Hills Residence.</a></li>
-        <li><a href="http://www.architecturalrecord.com/articles/11884-hollywood-hills-residence" target="_blank"><span>Architectural Record.</span> Hollywood Hills Residence.</a></li>
-        <li><a href="http://sfvbj.com/news/2015/apr/17/beverly-hills-developer-planning-sherman-oaks-smal/" target="_blank"><span>San Fernando Valley Business Journal.</span> Developer Planning Sherman Oaks Small-Lot Subdivision.</a></li>
-        <li><a href="http://www.architectmagazine.com/office-projects/class-a-office.aspx" target="_blank"><span>Architect.</span> Typology: Class A Office, rising profits, improved green standards, and sluggish hiring yield a mixed picture for Class A space.</a></li>
-        <li><a href="https://staff.aub.edu.lb/~webmgate/winter2009/notes.htm" target="_blank"><span>Maingate.</span> Hraztan Zeitlian appointment to CA Architects Board highlighted in Class Notes Section.</a></li>
+        <li><a href="http://www.dezeen.com/2016/10/15/struere-builds-hollywood-home-revive-mid-century-modernism-los-angeles-california/" target="_blank"><span>dezeen.</span> Struere builds Hollywood home to help revive mid-century modernism.<span class="pub-date">2016/10</span></a></li>
+        <li><a href="http://www.archdaily.com/795861/hollywood-hills-residence-struere?utm_medium=email&utm_source=ArchDaily%20List" target="_blank"><span>ArchDaily.</span> Hollywood Hills Residence.<span class="pub-date">2016/09</span></a></li>
+        <li><a href="http://www.architecturalrecord.com/articles/11884-hollywood-hills-residence" target="_blank"><span>Architectural Record.</span> Hollywood Hills Residence.<span class="pub-date">2016/08</span></a></li>
+        <li><a href="http://sfvbj.com/news/2015/apr/17/beverly-hills-developer-planning-sherman-oaks-smal/" target="_blank"><span>San Fernando Valley Business Journal.</span> Developer Planning Sherman Oaks Small-Lot Subdivision.<span class="pub-date">2015/04</span></a></li>
+        <li><a href="http://www.architectmagazine.com/office-projects/class-a-office.aspx" target="_blank"><span>Architect.</span> Typology: Class A Office, rising profits, improved green standards, and sluggish hiring yield a mixed picture for Class A space.<span class="pub-date">2011/03</span></a></li>
+        <li><a href="https://staff.aub.edu.lb/~webmgate/winter2009/notes.htm" target="_blank"><span>Maingate.</span> Hraztan Zeitlian appointment to CA Architects Board highlighted in Class Notes Section.<span class="pub-date">2009/04</span></a></li>
       </ul>
     </div>
   </section>
@@ -34,30 +34,30 @@ get_header();
     <div class="container">
       <!-- <h2>Print</h2> -->
       <ul>
-        <li><div><span>Los Angeles Business Journal.</span> Hraztan Zeitlian quoted in Question of the Week section about Electronic Media on Downtown Skyscrapers.</div></li>
-        <li><div><span>Los Angeles Business Journal.</span> Hraztan Zeitlian quoted in Question of the Week section about the market for qualified employees.</div></li>
-        <li><div><span>California Architects.</span> Hraztan Zeitlian appointment to CA Architects Board highlighted in New Board Members and Reappointments</div></li>
-        <li><div><span>Form.</span> Hraztan Authored ‘The New Library’ Article.</div></li>
-        <li><div><span>The Architects Newspaper.</span> Hraztan Quoted, Hilal, Paraha Featured.</div></li>
-        <li><div><span>Mark 11.</span> Praha Feaured.</div></li>
-        <li><div><span>Risd Views.</span> Hilal Featured.</div></li>
-        <li><div><span>Innovative Home.</span> Hilal Featured.</div></li>
-        <li><div><span>Prestige.</span> Praha Featured.</div></li>
-        <li><div><span>Le Moniteur.</span> Praha Featured.</div></li>
-        <li><div><span>Contract.</span> Nox Featured.</div></li>
-        <li><div><span>ARCCA.</span> Article on Struere.</div></li>
-        <li><div><span>Form.</span> Praha Featured.</div></li>
-        <li><div><span>Blinmisery.</span> Praha Featured.</div></li>
-        <li><div><span>The Armenian Reporter Arts & Culture.</span> Article on Praha.</div></li>
-        <li><div><span>Domus Web.</span> Hilal Featured.</div></li>
-        <li><div><span>Innovative Home.</span> Nox Featured.</div></li>
-        <li><div><span>Risd Views.</span> Nox Featured.</div></li>
-        <li><div><span>Prestige.</span> Hilal Featured.</div></li>
-        <li><div><span>Icon 39.</span> Nox Featured.</div></li>
-        <li><div><span>AIA National Website</span></li>
-        <li><div><span>Glass Magazine.</span> Nox Featured.</div></li>
-        <li><div><span>Domus 898.</span> Hilal Featured.</div></li>
-        <li><div><span>LA Architect.</span> Nox Featured.</div></li>
+        <li><div><span>Los Angeles Business Journal.</span> Hraztan Zeitlian quoted in Question of the Week section about Electronic Media on Downtown Skyscrapers.<span class="pub-date">2011/04</span></div></li>
+        <li><div><span>Los Angeles Business Journal.</span> Hraztan Zeitlian quoted in Question of the Week section about the market for qualified employees.<span class="pub-date">2011/03</span></div></li>
+        <li><div><span>California Architects.</span> Hraztan Zeitlian appointment to CA Architects Board highlighted in New Board Members and Reappointments.<span class="pub-date">2009/05</span></div></li>
+        <li><div><span>Form.</span> Hraztan Authored ‘The New Library’ Article.<span class="pub-date">2005/08</span></div></li>
+        <li><div><span>The Architects Newspaper.</span> Hraztan Quoted, Hilal, Paraha Featured.<span class="pub-date">2008/03</span></div></li>
+        <li><div><span>Mark 11.</span> Praha Feaured.<span class="pub-date">2008/01</span></div></li>
+        <li><div><span>Prestige.</span> Praha Featured.<span class="pub-date">2007/12</span></div></li>
+        <li><div><span>Le Moniteur.</span> Praha Featured.<span class="pub-date">2007/11</span></div></li>
+        <li><div><span>Contract.</span> Nox Featured.<span class="pub-date">2007/11</span></div></li>
+        <li><div><span>ARCCA.</span> Article on Struere.<span class="pub-date">2007/11</span></div></li>
+        <li><div><span>Form.</span> Praha Featured.<span class="pub-date">2007/10</span></div></li>
+        <li><div><span>Blinmisery.</span> Praha Featured.<span class="pub-date">2007/09</span></div></li>
+        <li><div><span>The Armenian Reporter Arts & Culture.</span> Article on Praha.<span class="pub-date">2007/07</span></div></li>
+        <li><div><span>Risd Views.</span> Hilal Featured.<span class="pub-date">2007/03</span></div></li>
+        <li><div><span>Innovative Home.</span> Hilal Featured.<span class="pub-date">2007/03</span></div></li>
+        <li><div><span>Domus Web.</span> Hilal Featured.<span class="pub-date">2007/02</span></div></li>
+        <li><div><span>Prestige.</span> Hilal Featured.<span class="pub-date">2006/12</span></div></li>            
+        <li><div><span>Domus 898.</span> Hilal Featured.<span class="pub-date">2006/12</span></div></li>
+        <li><div><span>LA Architect.</span> Nox Featured.<span class="pub-date">2006/12</span></div></li>
+        <li><div><span>Innovative Home.</span> Nox Featured.<span class="pub-date">2006/11</span></div></li>
+        <li><div><span>Glass Magazine.</span> Nox Featured.<span class="pub-date">2006/10</span></div></li>
+        <li><div><span>AIA National Website</span><span class="pub-date">2006/10</span></div></li>
+        <li><div><span>Risd Views.</span> Nox Featured.<span class="pub-date">2006/09</span></div></li>
+        <li><div><span>Icon 39.</span> Nox Featured.<span class="pub-date">2006/09</span></div></li>
       </ul>
     </div>
   </section>
